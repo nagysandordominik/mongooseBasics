a71ee3e287670d5d236ed31d8cdba59ee7c28929
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-04-08 22:08:51 +0200

    add: new schema buildup with validation

--- index.js
+++ index.js
@@ -15,4 +15,39 @@ const movieSchema = new mongoose.Schema({
     score:Number
 });
 
-mongoose.model('Movie', movieSchema);
+const Movie = mongoose.model('Movie', movieSchema);
+
+Movie.insertMany([
+    {title: 'GOAL!', year: 2005, score: 9.0},
+    {title: 'Intouchables', year: 2011, score: 9.5},
+    {title: 'Üvegtigris', year: 2002, score: 4.0},
+    {title: 'Valami amerika', year: 2001, score: 3.0}
+])
+.then(data => {
+    console.log('It worked!');
+    console.log('data');
+})
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+const bike = new Product ({ name: 'Csepel Cruiser', price: 95000, color: 'black'})
+bike.save()
+    .then(data => {
+        console.log('It worked!');
+        console.log(data);
+    })
+    .catch(err => {
+        console.log('Error')
+        console.log(err)
+    })
